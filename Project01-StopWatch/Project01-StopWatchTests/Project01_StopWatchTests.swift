e2fa35827d332afc37bd38094c59a1739854c63c
space <910455361@qq.com>
2018-04-20 17:32:11 +0800

    project01上传
    project01上传

---
+++ Project01-StopWatch/Project01-StopWatchTests/Project01_StopWatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project01_StopWatchTests.swift
+//  Project01-StopWatchTests
+//
+//  Created by space on 2018/4/20.
+//  Copyright © 2018年 space. All rights reserved.
+//
+
+import XCTest
+@testable import Project01_StopWatch
+
+class Project01_StopWatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
