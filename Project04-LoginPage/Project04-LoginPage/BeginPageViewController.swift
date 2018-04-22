af8e9802678e30b558d246d61734eccd4e3d043a
space <910455361@qq.com>
2018-04-22 16:00:52 +0800

    创建项目4
    床架项目4，是login界面

---
+++ Project04-LoginPage/Project04-LoginPage/BeginPageViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BeginPageViewController.swift
+//  Project04-LoginPage
+//
+//  Created by space on 2018/4/22.
+//  Copyright © 2018年 space. All rights reserved.
+//
+
+import UIKit
+
+class BeginPageViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project04-LoginPage/Project04-LoginPage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project04-LoginPage
+//
+//  Created by space on 2018/4/22.
+//  Copyright © 2018年 space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
