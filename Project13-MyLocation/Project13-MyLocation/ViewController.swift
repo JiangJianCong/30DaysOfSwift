100a2ddb9da2645f76e9c2a7e50fd57433650f40
space <910455361@qq.com>
2018-05-06 15:16:36 +0800

    创建项目13-获取地址

---
+++ Project13-MyLocation/Project13-MyLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project13-MyLocation
+//
+//  Created by space on 2018/5/6.
+//  Copyright © 2018年 space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Project13-MyLocation/Project13-MyLocationTests/Project13_MyLocationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project13_MyLocationTests.swift
+//  Project13-MyLocationTests
+//
+//  Created by space on 2018/5/6.
+//  Copyright © 2018年 space. All rights reserved.
+//
+
+import XCTest
+@testable import Project13_MyLocation
+
+class Project13_MyLocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
